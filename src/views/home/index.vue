787e5c0acfa913524a1a6092fea6725da48c081c
panshiting <1030932605@qq.com>
2020-04-30 18:15:29 +0800

    删除注释

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,27 +1,53 @@
 <template>
-  <el-container class="container">
-    <div class="title">首页1111</div>
-    <el-header> 首页1111 </el-header>
-  </el-container>
+  <div class="container">
+    <el-button @click="handleClick">打开可拖动组件</el-button>
+    <div ref="iframeContent" class="iframe-content">
+      <iframe id="iframe" ref="iframe" src="https://www.baidu.com/" width="100%" />
+    </div>
+    <el-dialog
+      v-el-drag-dialog
+      title="弹窗"
+      :model="false"
+      :modal-append-to-body="false"
+      :append-to-body="true"
+      :close-on-click-modal="false"
+      :visible.sync="visible">
+      <div>你好！</div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
+import elDragDialog from '@/directive/el-drag-dialog'
 export default {
   name: 'Home',
   data () {
-    return {}
+    return {
+      visible: false
+    }
   },
-  methods: {}
+  directives: { elDragDialog },
+  mounted () {
+    this.setSize()
+  },
+  methods: {
+    handleClick () {
+      this.visible = true
+    },
+    setSize () {
+      const winHeight = window.innerHeight
+      console.log(window.innerHeight)
+      console.log(this.$refs.iframe.contentWindow.document.body.clientHeight)
+      this.$refs.iframe.height = (winHeight -40) + 'px'
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 .container {
   width: 100%;
-  height: 100%;
+  height: 100vh;
   background-color: rgba(#000, .5);
-  .title {
-    color: red;
-  }
 }
 </style>
